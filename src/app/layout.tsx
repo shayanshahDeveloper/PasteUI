43e906acad6525c3433b5b80651169b447b54e40
Shayan Shah <shahaankhan138@gmail.com>
2026-03-09 14:08:16 +0500

    verified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,14 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* Google AdSense */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1556719196816220"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
       {/* Google Analytics */}
       <Script
         strategy="afterInteractive"
